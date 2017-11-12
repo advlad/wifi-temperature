c9ebc0d26b240d4d562d034239a9eb065bf9198d
Vlad Adomnicai <vlad.adomnicai@gmail.com>
2017-11-12 23:13:09 +0200

    Add temperature reading test

--- main/main.c
+++ main/main.c
@@ -1,6 +1,24 @@
 #include "stdio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "string.h"
+#include "esp_system.h"
+#include "nvs_flash.h"
+#include "ds18b20.h"
 
-void app_main(void)
+const int DS_PIN = 14;
+// *** Task measuring temperature
+void mainTask(void *pvParameters)
 {
-  printf("Hello world\n");
+    ds18b20_init(DS_PIN);
+    while (1)
+    {
+        printf("Temperature: %0.1f\n",ds18b20_get_temp());
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+void app_main()
+{
+    nvs_flash_init();
+    xTaskCreatePinnedToCore(&mainTask, "mainTask", 2048, NULL, 5, NULL, 0);
 }
